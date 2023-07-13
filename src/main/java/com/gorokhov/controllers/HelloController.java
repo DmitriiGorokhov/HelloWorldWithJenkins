7330b181ce6dfdf5fff10ca0c5ab434e5d6abc47
Dmitrii Gorokhov <gordi2012@gmail.com>
2023-07-13 06:51:01 +0300

    first commit

---
+++ src/main/java/com/gorokhov/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.gorokhov.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greeting() {
+        return "hello";
+    }
+}
